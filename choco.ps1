f7280ac439ca53f50a05a8706f7e26cbdac697c7
Simen <zimen2004@gmail.com>
2023-03-06 23:54:24 +0100

    Init commit

---
+++ choco.ps1
@@ -0,0 +1,8 @@
+#     # Change execution policy
+# Set-ExecutionPolicy Bypass -Scope Process 
+#     # Install chocolatey
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    # Display installed chocolatey version
+choco -v
+    # 
+# choco install ./choco.config
