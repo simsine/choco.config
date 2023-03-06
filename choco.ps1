2f2d65c1e2e111133e7c30af553bba9d03cc7ac5
Simen <zimen2004@gmail.com>
2023-03-07 00:13:44 +0100

    Updated comments

--- choco.ps1
+++ choco.ps1
@@ -1,8 +1,10 @@
-    # Change execution policy
+#! MUST RUN WITH POWERSHELL AS ADMINISTRATOR !#
+
+# Change execution policy
 Set-ExecutionPolicy Bypass -Scope Process 
-    # Install chocolatey
+# Install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    # Display installed chocolatey version
+# Display installed chocolatey version
 choco -v
-    # 
+# Install choco packages from config file
 choco install ./choco.config
