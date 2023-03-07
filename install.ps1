3f5241b256621037eed38d559e17fac0ac92e6f0
Simen <zimen2004@gmail.com>
2023-03-07 01:23:19 +0100

    Added tests and prompts

---
+++ install.ps1
@@ -0,0 +1,25 @@
+#! MUST RUN WITH POWERSHELL AS ADMINISTRATOR !#
+
+Write-Output "Checking if chocolatey is installed..."
+try {
+    choco.exe
+    Write-Output "Chocolatey is installed."
+}
+catch{
+    Write-Output "Chocolatey is not installed, attempting install"
+
+    # Change execution policy
+    Set-ExecutionPolicy Bypass -Scope Process 
+    # Install chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    # Display installed chocolatey version
+    choco.exe
+}
+$prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
+
+if ($prompt -eq "y") {
+    # Install choco packages from config file
+    choco install .\config\choco.config
+}else{
+    Write-Output "Install canceled, exiting."
+}
