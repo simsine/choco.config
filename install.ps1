227be53bdee9c8feb3c1f04ff9cc8693a49b220f
Simen <zimen2004@gmail.com>
2023-03-08 21:54:25 +0100

    Added more comments and try statements

--- install.ps1
+++ install.ps1
@@ -2,24 +2,28 @@
 
 Write-Output "Checking if chocolatey is installed..."
 try {
+    # Check if chocolatey is installed correctly
     choco.exe
     Write-Output "Chocolatey is installed."
-}
-catch{
+}catch{
+    # Conclude chocolatey is not installed correctly
     Write-Output "Chocolatey is not installed, attempting install"
-
-    # Change execution policy
-    Set-ExecutionPolicy Bypass -Scope Process 
-    # Install chocolatey
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    try{
+        # Attempt to install chocolatey
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    }catch{
+        Write-Output "Failed to install chocolatey  "
+        Exit
+    }
     # Display installed chocolatey version
     choco.exe
 }
-$prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
 
+$prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
 if ($prompt -eq "y") {
     # Install choco packages from config file
-    choco install .\config\choco.config
+    choco.exe install .\config\choco.config
 }else{
     Write-Output "Install canceled, exiting."
+    Exit
 }
