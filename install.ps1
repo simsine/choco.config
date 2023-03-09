d72aa1ae01e360409eaa6f8d8f4999dd4c1e7ac9
Simen <zimen2004@gmail.com>
2023-03-09 22:02:22 +0100

    Updated install comments

--- install.ps1
+++ install.ps1
@@ -2,24 +2,23 @@
 
 Write-Output "Checking if chocolatey is installed..."
 try {
+    # Check for chocolatey installation
     choco.exe
     Write-Output "Chocolatey is installed."
 }
 catch{
-    Write-Output "Chocolatey is not installed, attempting install"
+    Write-Output "Chocolatey is not installed, attempting install..."
 
-    # Change execution policy
-    Set-ExecutionPolicy Bypass -Scope Process 
-    # Install chocolatey
+    # Change execution policy and install chocolatey
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     # Display installed chocolatey version
     choco.exe
 }
-$prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
 
+$prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
 if ($prompt -eq "y") {
     # Install choco packages from config file
-    choco install .\config\choco.config
+    choco.exe install .\config\choco.config
 }else{
     Write-Output "Install canceled, exiting."
 }
