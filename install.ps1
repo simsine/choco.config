90b1c1ffbf9051a3da807cfb2d50c012d12a0fc7
simsine <zimen2004@gmail.com>
2023-07-07 00:09:47 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@
 Write-Output "Checking if chocolatey is installed..."
 try {
     # Check if chocolatey is installed correctly
-    choco.exe
+    choco.exe --verbose
     Write-Output "Chocolatey is installed."
 }catch{
     # Conclude chocolatey is not installed correctly
@@ -13,15 +13,15 @@ try {
         # Attempt to install chocolatey
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
     }catch{
-        Write-Output "Failed to install chocolatey  "
+        Write-Output "Failed to install chocolatey, exiting"
         Exit
     }
     # Display installed chocolatey version
-    choco.exe
+    choco.exe --verbose
 }
 
 $prompt = Read-Host -Prompt "Do you wish to install the packages listed in the choco.config file? | [y]es / [n]o"
-if ($prompt -eq "y") {
+if ($prompt -eq "y" -or "Y" -or "yes" -or "YES") {
     # Install choco packages from config file
     choco.exe install .\config\choco.config
 }else{
